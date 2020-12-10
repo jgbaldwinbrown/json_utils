b4de8d366fd50a736736397376e58a5b34b6f07e
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-12-09 17:40:51 -0700

    fq2json

---
+++ fq2json.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+def main():
+    out = []
+    header = ""
+    seq = ""
+    qual = ""
+    for i, l in enumerate(sys.stdin):
+        l = l.rstrip('\n')
+        if i%4 == 0:
+            header = l[1:]
+        if i%4 == 1:
+            seq = l
+        if i%4 == 3:
+            qual = l
+            out.append({"header": header, "seq": seq, "qual": qual})
+    print(json.dumps(out, indent=4, sort_keys=True))
+
+if __name__ == "__main__":
+    main()
