fa70f52f9acd1d1b3d953c2e5f68ba064f1efd51
James Baldwin-Brown <jgbaldwinbrown@gmail.com>
2020-12-09 17:40:55 -0700

    sortj

---
+++ sortj.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+def main():
+    indata = json.load(sys.stdin)
+    sort_text = sys.argv[1]
+    sort_func_text = "lambda x: " + sort_text
+    sort_func = eval(sort_func_text)
+    out = sorted(indata, key = sort_func)
+    print(json.dumps(out, indent=4, sort_keys=True))
+
+if __name__ == "__main__":
+    main()
